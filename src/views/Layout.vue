c0af3f9c47e507134f7453e879b5b68af799cacd
wangbo <bo.wang@mycool.tv>
2026-04-01 10:34:16 +0800

    Sidebar 优化

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -34,17 +34,3 @@ html, body, #app {
   margin: 0;
 }
 </style>
-
-
-<!--<template>
-  <div class="common-layout">
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
-  </div>
-</template>
--->
